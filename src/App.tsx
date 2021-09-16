eb21830a4a2697c67d201b22e300549a362f4d28
Gabriel Leon <gabriel.leon@baritecnologia.com.br>
2021-09-16 13:27:11 -0300

    refactor: isolate content

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import './styles/global.scss';
 import './styles/sidebar.scss';
 import './styles/content.scss';
 import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
 
 export interface GenreResponseProps {
   id: number;
@@ -20,7 +21,7 @@ export interface GenreResponseProps {
   title: string;
 }
 
-interface MovieProps {
+export interface MovieProps {
   imdbID: string;
   Title: string;
   Poster: string;
@@ -62,20 +63,7 @@ export function App() {
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
       <SideBar genres={genres} onClick={handleClickButton} selectedGenreId={selectedGenreId} />
-
-      <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
-      </div>
+      <Content movies={movies} selectedGenre={selectedGenre} />
     </div>
   )
 }
